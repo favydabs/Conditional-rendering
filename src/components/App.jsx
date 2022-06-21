091d506cfd2278bf8decb805857e453170a166b7
favydabs <favourdabit@gmail.com>
2022-06-21 08:30:49 +0100

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 var isLoggedIn = true;
 function renderConditionally() {
-  if (isLoggedIn===true) {
-      return  <h1>Hello</h1>
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>
 
   } else {
-    return(
+    return (
       <form className="form">
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button type="submit">Login</button>
-    </form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
     );
   }
 
 }
 function App() {
-    <div className="container" />
-     {renderConditionally()}
+  <div className="container" />
+  { renderConditionally() }
 }
 
 export default App;
