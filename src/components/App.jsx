a35f9b97e9e8f6766f08a5be29fec9189918360a
favydabs <favourdabit@gmail.com>
2022-06-21 09:48:55 +0100

    third commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,13 @@ import Login from './Login';
 var isLoggedIn = false;
 
 
-const CurrentTime = new Date().getHours;
+const CurrentTime = new Date(2022, 21, 6, 10).getHours();
 
 function App() {
   return <div className="container">{
     //Ternary operator//
     // isLoggedIn ? <h1>Hello</h1>: <Login />
-    CurrentTime > 17 ? <h1>You need to rest</h1> : <h2>Resume work</h2>
+    CurrentTime > 12 ? <h1>Resume work</h1> : <h2>You need to rest</h2>
   } </div>;
 }
 
