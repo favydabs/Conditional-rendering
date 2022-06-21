12b4ca7e2c374360e19efdbb03f813d537dfb09c
favydabs <favourdabit@gmail.com>
2022-06-21 09:22:46 +0100

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import Login from './Login';
 
 var isLoggedIn = false;
-function renderConditionally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
 
-}
+
+const CurrentTime = new Date().getHours;
+
 function App() {
-  return <div className="container">
-    {renderConditionally()}
-  </div>;
+  return <div className="container">{
+    //Ternary operator//
+    // isLoggedIn ? <h1>Hello</h1>: <Login />
+    CurrentTime > 17 ? <h1>You need to rest</h1> : <h2>Resume work</h2>
+  } </div>;
 }
 
 export default App;
